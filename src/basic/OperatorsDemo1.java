0aee30c6276bb9f4ae1a885d4afd2e9591705a60
Resha C <reshakchettry@gmail.com>
2025-08-28 14:01:24 -0400

    Add files via upload

---
+++ src/basic/OperatorsDemo1.java
@@ -0,0 +1,14 @@
+package day4.basic;
+
+public class OperatorsDemo1 {
+    public static void main(String[] args) {
+
+        //1.Arithmetic Operators + - * / %
+        System.out.println(7/3);
+        System.out.println(7%3);
+
+        int a = 4 + 5 - 9 / 3 * 7 - 1 % 2;
+        System.out.println(a);
+
+    }
+}
