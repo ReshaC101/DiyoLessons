62faf7ff9a37516195f2afdf2c19a33015e2ccea
Resha C <reshakchettry@gmail.com>
2025-08-28 13:32:38 -0400

    Add files via upload

---
+++ src/day3/basic/DataTypeDemo1.java
@@ -0,0 +1,14 @@
+package day3.basic;
+
+public class DataTypeDemo1 {
+    public static void main(String[] args) {
+        byte b1 = 127;
+        byte b2 = -128;
+
+        short s1= 32767;
+        short s2= -32768;
+
+
+
+    }
+}
